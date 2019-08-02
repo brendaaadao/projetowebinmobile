a2831d4d83b1142c7d25343322c3ff08e45fa08e
brendaadao <brendaadao@brq.com>
2019-08-02 00:29:10 -0300

    Exemplo de departamentos

---
+++ ProjetoWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoWebMVC.Models;
+
+namespace ProjetoWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Recursos Humanos" });
+            list.Add(new Departamento { Id = 2, Nome = "Marketing" });
+            list.Add(new Departamento { Id = 3, Nome = "Financeiro" });
+
+            return View(list);
+        }
+    }
+}
